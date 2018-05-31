cd1c250a1e6bf02e1352f9518f4e7c3fa2909ea3
M Taufik Hidayat <12mtauhid@gmail.com>
2018-05-31 07:56:27 +0700

    Tampilan Home
    engatur Link dan membagi file di dalam view

--- application/controllers/c_home.php
+++ application/controllers/c_home.php
@@ -7,7 +7,12 @@
 		}
 
     	public function index(){
+
+        	$this->load->view('template/header');
         	$this->load->view('v_home');
+        	$this->load->view('template/sidebar');
+        	$this->load->view('template/content');
+        	$this->load->view('template/footer');
         }
     }
 ?>
---
+++ application/views/template/sidebar.php
@@ -0,0 +1,93 @@
+<div id='wrapper'>
+      <div id='main-nav-bg'></div>
+      <nav id='main-nav'>
+        <div class='navigation'>
+          <ul class='nav nav-stacked'>
+            <li class='active'>
+              <a href='index-2.html'>
+                <i class='icon-dashboard'></i>
+                <span>Dashboard</span>
+              </a>
+            </li>
+            <li class=''>
+                      <a class="dropdown-collapse" href="#"><i class='icon-user'></i>
+              <span>Dosen</span>
+              <i class='icon-angle-down angle-down'></i>
+              </a>
+      
+              <ul class='nav nav-stacked'>
+                <li class=''>
+                  <a href='#'>
+                    <i class='icon-caret-right'></i>
+                    <span>Data Dosen</span>
+                  </a>
+                </li>
+                <li class=''>
+                  <a href='form_components.html'>
+                    <i class='icon-caret-right'></i>
+                    <span>Rekomendasi Judul</span>
+                  </a>
+                </li>
+                <li class=''>
+                  <a href='c_hakAksesDosen'>
+                    <i class='icon-caret-right'></i>
+                    <span>Hak Akses</span>
+                  </a>
+                </li>
+                <li class=''>
+                  <a href='wizard.html'>
+                    <i class='icon-caret-right'></i>
+                    <span>Pengajuan Judul</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a class='dropdown-collapse ' href='#'>
+                <i class='icon-user'></i>
+                <span>Mahasiswa</span>
+                <i class='icon-angle-down angle-down'></i>
+              </a>
+              <ul class='nav nav-stacked'>
+                <li class=''>
+                  <a href='ui_elements.html'>
+                    <i class='icon-caret-right'></i>
+                    <span>Data Mahasiswa</span>
+                  </a>
+                </li>
+                <li class=''>
+                  <a href='widgets.html'>
+                    <i class='icon-caret-right'></i>
+                    <span>Pengajuan Judul</span>
+                  </a>
+                </li>
+                <li class=''>
+                  <a href='widgets.html'>
+                    <i class='icon-caret-right'></i>
+                    <span>Pengajual Judul Final</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class=''>
+              <a href='buttons_and_icons.html'>
+                <i class='icon-cog'></i>
+                <span>Pengaturan</span>
+              </a>
+            </li>
+            
+            <li class=''>
+              <a href='tables.html'>
+                <i class='icon-bar-chart'></i>
+                <span>Statistik</span>
+              </a>
+            </li>
+            <li class=''>
+              <a href='tables.html'>
+                <i class='icon-file'></i>
+                <span>Laporan</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
