1f7f27142b97491755d28e0c3cbc05743514ee2c
Asus-PC\Asus <lailysarifa@gmail.com>
2018-06-24 21:17:03 +0700

    mahasiswa
    mahasiswa polosan

---
+++ application/views/v_inputjudulmhs.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>INPUT JUDUL MAHASISWA</title>	
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/style.css">
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<hgroup>
+				<h1>MAHASISWA</h1>
+			</hgroup>
+			<nav>
+				<ul>
+					<li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+					<li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li>
+				</ul>
+			</nav>
+			<div class="clear"></div>
+		</header>
+		<section>
+		<h1><?php echo $judul ?></h1>
+		<p>
+			
+		</p>
+		</section>
+		<footer>						
+		
+		</footer>
+	</div>
+</body>
+</html>
