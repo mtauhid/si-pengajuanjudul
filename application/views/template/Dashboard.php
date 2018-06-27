3dea4b11548aff7923b5ab4a3f4e1b7b29782c71
M Taufik Hidayat <12mtauhid@gmail.com>
2018-06-28 06:30:32 +0700

    Dashboard

--- application/views/template/Dashboard.php
+++ application/views/template/Dashboard.php
@@ -8,26 +8,11 @@
                   <i class='icon-dashboard'></i>
                   <span>Dashboard</span>
                 </h1>
-                <div class='pull-right'>
-                  <div class='btn-group'>
-                    <a class="btn btn-white hidden-xs" href="#">Last month</a>
-                    <a class="btn btn-white" href="#">Last week</a>
-                    <a class="btn btn-white " href="#">Today</a>
-                          <a class="btn btn-white" id="daterange" href="#"><i class='icon-calendar'></i>
-                    <span>Custom</span>
-                    <b class='caret'></b>
-                    </a>
-              
-                  </div>
-                </div>
               </div>
               <div class='alert alert-info alert-dismissable'>
                 <a class='close' data-dismiss='alert' href='#'>&times;</a>
-                Welcome to
-                <strong>Flatty (v2.2)</strong>
-                - I hope you'll like it. Don't forget - you can change theme color in top right corner
-                <i class='icon-cog'></i>
-                if you want.
+                Selamat Datang, di 
+                <strong>Sistem Informasi Pengajuan Judul Tugas Akhir</strong> Jurusan Teknologi Informasi Politeknik Negeri Jember
               </div>
               <div class='row'>
                 <div class='col-sm-6 box'>
